79398900e2b458339f7eb666866160cbb1783ba2
bhdhruv <bhdhruv@gmail.com>
2025-03-14 19:33:55 -0400

    testing commit for CI/CD

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,15 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "hi this is a neew commit!" );
+        
+    }
+}
